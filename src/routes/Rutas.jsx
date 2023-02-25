7c3293499d2975da2f88c9c08684da6a81c27dc0
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-02-25 00:04:16 -0300

    Add routes and components

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Route, Routes} from 'react-router-dom'
 import Home from '../views/Home'
 import NotFound from '../views/NotFound'
+import Projects from '../views/Projects'
+import Skills from '../views/Skills'
 
 
 const Rutas = () => {
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<NotFound />} />
+    <Route path = "/" element = { <Home/> } />
+    <Route path = "*" element = { <NotFound/> } />
+    <Route path = '/projects' element = { <Projects/> } />
+    <Route path = '/skills' element = { <Skills/> } />
   </Routes>
   )
 }
