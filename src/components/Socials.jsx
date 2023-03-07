4ef782b7034244d7d01a5d0f48c54aad53b0acf1
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-03-07 00:44:23 -0300

    Add CV

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -17,7 +17,9 @@ const Socials = () => {
       <li className='social__li social__li--noresponsive'>
       <motion.a
         className='social__a'
-        href="mailto:gz.ramses.oc@gmail.com?subject=Contacto%20Laboral%20"
+        href="https://www.canva.com/design/DAFcYzCo_eI/9QW3c8EfMB1lt29Aqtu6lQ/view?utm_content=DAFcYzCo_eI&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+        target="_blank" 
+        rel='noreferrer'
         activeClassName="active"
         whileTap={{scale: 0.90}}
         whileHover={{scale: 1.05, border: "1px solid #F2F2F2", transition: "all 1s ease-in-out"}}
